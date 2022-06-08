c908cf31c1211d9fa04fb9a71e90ade2241ba5c9
Lazar Narcis <lnarcis310@gmail.com>
2022-06-08 12:15:40 +0300

    bug fixes

--- app.js
+++ app.js
@@ -32,42 +32,16 @@ function generateRandomDiv () {
     setTimeout(generateRandomDiv, time);
 }
 
-function alignItem (div) {
-    let randomHeight = Math.floor((Math.random() * 2) + 1);
-    let randomWidth = Math.floor((Math.random() * 2) + 1);
-
-    let windowWidth = window.innerWidth / 2;
-    let windowHeight = window.innerHeight / 2;
+function moveElement (div) {
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
     let randomNumberForTop = generateRandomNumber(1, windowHeight - 75);
     let randomNumberForLeft = generateRandomNumber(1, windowWidth - 75);
 
-    if (randomHeight == 1) {
-        //left
-        if (randomWidth == 1) {
-            //bottom
-            div.style.marginLeft = randomNumberForTop + "px";
-            div.style.marginBottom = randomNumberForLeft + "px";
-        } else {
-            //top
-            div.style.marginLeft = randomNumberForTop + "px";
-            div.style.marginTop = randomNumberForLeft + "px";
-        } 
-    } else {
-        //right
-        if (randomWidth == 1) {
-            //bottom
-            div.style.marginRight = randomNumberForTop + "px";
-            div.style.marginBottom = randomNumberForLeft + "px";
-        } else {
-            //top
-            div.style.marginRight = randomNumberForTop + "px";
-            div.style.marginTop = randomNumberForLeft + "px";
-        } 
-    }
-}
-
-function moveElement (div) {
-    setTimeout(alignItem(div), time);
+    setTimeout(function () {
+        div.style.marginTop = randomNumberForTop + "px";
+        div.style.marginLeft = randomNumberForLeft + "px";
+    }, time);
 }
 
 principal.addEventListener("click", generateRandomDiv);
